1080ab2d7b339ccea8e479d456a0faa6c0ef4901
hachi <519619758@qq.com>
2022-09-01 17:19:47 +0800

    2022.9.1 17:20 LC2102

--- L2102_SORTracker.cpp
+++ L2102_SORTracker.cpp
@@ -8,16 +8,17 @@
 using namespace std;
 
 // 大顶堆
-struct cmp_max{
-    bool operator ()(pair<int, string>& a, pair<int, string>& b){
+struct cmp_min{
+    // a < b ====> true
+    bool operator ()(const pair<int, string>& a, const pair<int, string>& b){
         if(a.first == b.first)  return a.second > b.second;
         return a.first < b.first;
     }
 };
 
 // 小顶堆
-struct cmp_min{
-    bool operator() (pair<int, string>& a, pair<int, string>& b){
+struct cmp_max{
+    bool operator() (const pair<int, string>& a, const pair<int, string>& b){
         if(a.first == b.first) return a.second < b.second;
         return a.first > b.first;
     }
@@ -27,35 +28,38 @@ class SORTracker{
 private:
     int queryCount; //调用get()时是第几次查询
 public:
-    priority_queue<pair<int, string> , vector<pair<int, string>>, cmp_min> minHeap;
-    priority_queue<pair<int, string> , vector<pair<int, string>>, cmp_max> maxHeap;
+    priority_queue<pair<int, string> , vector<pair<int, string>>, cmp_max> minHeap;
+    priority_queue<pair<int, string> , vector<pair<int, string>>, cmp_min> maxHeap;
 
     SORTracker() {
-        queryCount = 1;
     }
 
+/*      可以维护小根堆的大小为 get() 的次数，怎样去维护呢？
+        添加值的时候先放到小根堆里面，此时小根堆的大小比get() 的次数多一；弹出小根堆的堆顶放到大根堆里，此时小根堆的大小与get() 的次数一样
+*/
     void add(string name, int score) {
-        maxHeap.emplace(score, name);
-
-        if( minHeap.size() < queryCount && maxHeap.top() < minHeap.top()){
-            cout << maxHeap.size() << endl;
-            minHeap.emplace(maxHeap.top());
-            maxHeap.pop();
-            cout << "添加{" << name + ", " + to_string(score) << "}到小根堆中" << endl;
-        }else{
-            cout << "添加{" << name + ", " + to_string(score) << "}到大根堆中" << endl;
+        // 只有当
+        if(!minHeap.empty() && cmp_min()(minHeap.top(), {score, name})){
+            maxHeap.emplace(minHeap.top());
+            minHeap.pop();
+            minHeap.emplace(score, name);
+        }else{ // minHeap.empty() || minHeap.top > {score, name}
+            maxHeap.emplace(score, name);
         }
     }
 
     string get() {
-        string res = minHeap.top().second;
-        minHeap.emplace(maxHeap.top());
+        // 每查询一次将大根堆堆顶元素输出并放到小根堆中，那么
+        // 查询后：小根堆的元素数 == 查询次数
+        // 查询前：小根堆的元素数 == 查询次数 - 1
+        minHeap.emplace(maxHeap.top()); // 大根堆堆顶加入小根堆
         maxHeap.pop();
-        queryCount ++;
+        string res = minHeap.top().second;
         cout << "第" + to_string(queryCount - 1) + "次查询，查询结果景点为 " << res << endl;
         return res;
     }
 };
+
 
 int main(){
 //    SORTracker* sor = new SORTracker();
@@ -64,7 +68,7 @@ int main(){
     SORTracker* tracker = new SORTracker(); // 初始化系统
     tracker->add("bradford", 2); // 添加 name="bradford" 且 score=2 的景点。
     tracker->add("branford", 3); // 添加 name="branford" 且 score=3 的景点。
-    tracker->get();              // 从好带坏的景点为：branford ，bradford 。
+    tracker->get();              // 从好到坏的景点为：branford ，bradford 。
     // 注意到 branford 比 bradford 好，因为它的 评分更高 (3 > 2) 。
     // 这是第 1 次调用 get() ，所以返回最好的景点："branford" 。
     tracker->add("alps", 2);     // 添加 name="alps" 且 score=2 的景点。
